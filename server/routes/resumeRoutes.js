4fcea5919e358d289314c603e56a5ab9df95f361
Devansh Rajput <rajputdevansh012@gmail.com>
2025-10-16 21:38:39 +0530

    Add Multer & ImageKit

---
+++ server/routes/resumeRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createResume,
+  getResumeById,
+  updateResume,
+  deleteResume,
+  getPublicResumeById,
+} from "../controllers/resumeController.js";
+import protect from "../middleware/authMiddleware.js";
+import upload from "../config/multer.js";
+
+const resumeRouter = express.Router();
+
+resumeRouter.post("/create", protect, createResume);
+resumeRouter.put("/update", upload.single("image"), protect, updateResume);
+resumeRouter.delete("/delete/:resumeId", protect, deleteResume);
+resumeRouter.get("/get/:resumeId", protect, getResumeById);
+resumeRouter.get("/public/:resumeId", getPublicResumeById);
+
+export default resumeRouter;
